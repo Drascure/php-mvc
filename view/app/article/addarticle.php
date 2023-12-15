9c6a6d245f2ea5819eef3e120d25b487569d2958
Benjamin VALLERAN <benjamin.valleran@hotmail.fr>
2023-12-15 11:57:48 +0100

    Ajout d'un formulaire d'un article

--- view/app/article/addarticle.php
+++ view/app/article/addarticle.php
@@ -1,4 +1,14 @@
-<?php 
-$this->dd($formAdd);
-
-?>
+<section>
+    <h3>Ajout d'un article</h3>
+    <form method="POST">
+        <?= $formAdd->label("titre") ?>
+        <?= $formAdd->input("titre") ?>
+        <?= $formAdd->error("titre") ?>
+        <br>
+        <?= $formAdd->label("contenu") ?>
+        <?= $formAdd->textarea("contenu") ?>
+        <?= $formAdd->error("contenu") ?>
+        <br>
+        <?= $formAdd->submit("", "Ajouter article") ?>
+    </form>
+</section>
