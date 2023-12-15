59e54c2ac42974d900917bc117d931529d96ec59
Benjamin VALLERAN <benjamin.valleran@hotmail.fr>
2023-12-15 16:33:56 +0100

    Ajout d'une méthode pour trier les articles

--- app/Controller/ArticleController.php
+++ app/Controller/ArticleController.php
@@ -17,7 +17,7 @@ class ArticleController extends Controller
     public function index()
     {
 
-        $articles = PostModel::all();
+        $articles = PostModel::allOrder("DESC");
         $user = new UserModel;
         $nbArticles = PostModel::count();
 
--- view/app/article/index.php
+++ view/app/article/index.php
@@ -8,7 +8,7 @@
 <h3><a href="<?= $view->path('article', [$article->id]); ?>"><?=$article->title?></a></h3>
 
 <p>
-    <strong>Publiée par :</strong> <?=$user->findById($article->author)->firstname . ' ' . $user->findById($article->author)->lastname?> le <?=$article->modifiedAt?>
+    <strong>Publiée par :</strong> <?=$user->findById($article->author)->firstname . ' ' . $user->findById($article->author)->lastname?> le <?=$article->createdAt?>
 </p>
 </article>
 
